8d1c630e55cb3c894b3f7524be97e1ed24b2de40
Jonathan Puide <jonte@slacktop>
2023-12-30 20:08:19 +0100

    Amogus
     Abobus

--- lua/plugs/lsp.lua
+++ lua/plugs/lsp.lua
@@ -3,7 +3,7 @@ local lspconfig = require('lspconfig')
 lspconfig.pyright.setup {}
 lspconfig.tsserver.setup {}
 require 'lspconfig'.clangd.setup{}
-
+require 'lspconfig'.zls.setup{}
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
